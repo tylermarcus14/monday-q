08dc4b0a8c6cc5a7533001f16ad55891a8b7ecc9
tylermarcus14 <42244293+tylermarcus14@users.noreply.github.com>
2020-03-08 12:04:10 -0400

    updated bluegolf scraping

--- src/utils/scrape-test.js
+++ src/utils/scrape-test.js
@@ -5,7 +5,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 
-var url = "https://sflpga.bluegolf.com/bluegolf/sflpga20/event/sflpga207/contest/1/leaderboard.htm";
+var url = "https://agpts.bluegolf.com/bluegolf/agws19/event/agws199/contest/1/leaderboard.htm";
 
 
 axios.get(url).then(function(response) {
@@ -16,7 +16,7 @@ axios.get(url).then(function(response) {
     $("tr").each(function(i, element) {
       var position = $(element).find("td.pos").slice(0).eq(0).text();
       var name = $(element).find("td.name>a>span.d-none.d-md-inline").slice(0).eq(0).text();
-      var thru = $(element).find("td").slice(3).eq(0).text();
+      var thru = $(element).find("td.thru").slice(0).eq(0).text();
       var score = $(element).find("td").slice(4).eq(0).text();
 
       if (position) {
