0dafe6150dd59f93bf3d7a6fcaa62a9f2d5510b1
tylermarcus14 <42244293+tylermarcus14@users.noreply.github.com>
2020-03-08 22:10:39 -0400

    added west fl golf to scraping

--- src/classes/AppRouter.js
+++ src/classes/AppRouter.js
@@ -771,6 +771,46 @@ class AppRouter {
 		  });
 		}
 
+		else if (site == "westfl") {
+		var db = mongojs(databaseUrl, collections);
+		db.on("error", function(error) {
+		console.log("Database Error:", error);
+		});
+		axios.get(url).then(function(response) {
+			// Load the html body from axios into cheerio
+			var $ = cheerio.load(response.data);
+	
+			$("#ctl00_MainContent_gvPursePlayersResults>tbody>tr").each(function(i, element) {
+				if (i === 0) return true;
+				var position = $(element).find("td").slice(0).eq(0).text().trim();
+				var name = $(element).find("td").slice(1).eq(0).text().trim();
+				var thru = "18";
+				var score = $(element).find("td").slice(4).eq(0).text().trim();
+		
+			  if (position) {
+				// Insert the data in the scrapedData db
+				db.results.insert({
+					title: req.body.title,
+					position: position,
+					name: name,
+					thru: thru,
+					score: score      
+				  },
+				function(err, inserted) {
+				  if (err) {
+					// Log the error if one is encountered during the query
+					console.log(err);
+				  }
+				  else {
+					// Otherwise, log the inserted data
+					console.log("Scraped");
+				  }
+				});
+			  }
+			});
+		  });
+		}
+
 
 			return res.redirect('/scrapes');
 
--- src/utils/scrape-test.js
+++ src/utils/scrape-test.js
@@ -5,33 +5,35 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 
-var url = "https://gprotour.bluegolf.com/bluegolfw/gprotour20/event/gprotour202/contest/1/leaderboard.htm";
+var url = "http://www.westfloridagolftour.com/TournamentResult.aspx?TournamentID=2075";
 
 
 axios.get(url).then(function(response) {
-    // Load the html body from axios into cheerio
-    var $ = cheerio.load(response.data);
-    // An empty array to save the data that we'll scrape
+  // Load the html body from axios into cheerio
+  var $ = cheerio.load(response.data);
 
-    $("tr").each(function(i, element) {
-      var position = $(element).find("td.pos").slice(0).eq(0).text();
-      var name = $(element).find("td.name>a>span.d-none.d-md-inline").slice(0).eq(0).text();
-      var thru = $(element).find("td.thru").slice(0).eq(0).text();
-      var score = $(element).find("td").slice(3).eq(0).text();
+  // An empty array to save the data that we'll scrape
 
-      if (position) {
-        // Insert the data in the scrapedData db
-        console.log(position,name,thru,score),
-        function(err, inserted) {
-          if (err) { defaultStatus
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log("Scraped");
-          }
-        };
-      }
-    });
+  $("#ctl00_MainContent_gvPursePlayersResults>tbody>tr").each(function(i, element) {
+    if (i === 0) return true;
+    var position = $(element).find("td").slice(0).eq(0).text().trim();
+    var name = $(element).find("td").slice(1).eq(0).text().trim();
+    var thru = "18";
+    var score = $(element).find("td").slice(4).eq(0).text().trim();
+
+    if (position) {
+      // Insert the data in the scrapedData db
+      console.log(position,name,thru,score),
+      function(err, inserted) {
+        if (err) {
+          // Log the error if one is encountered during the query
+          console.log(err);
+        }
+        else {
+          // Otherwise, log the inserted data
+          console.log("Scraped");
+        }
+      };
+    }
   });
+});
